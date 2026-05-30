120aae85dfa46908c3e97f5b1ba1a03d7ac65fdd
wasifjhojha-boop <wasifjhojha@gmail.com>
2026-05-30 19:42:01 +0530

    Refactor webpack alias configuration

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,14 +2,8 @@ const path = require('path');
 
 module.exports = {
   webpack: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    },
     configure: (webpackConfig) => {
-      webpackConfig.resolve.alias = {
-        ...webpackConfig.resolve.alias,
-        '@': path.resolve(__dirname, 'src')
-      };
+      webpackConfig.resolve.alias['@'] = path.resolve(__dirname, 'src');
       return webpackConfig;
     }
   }
