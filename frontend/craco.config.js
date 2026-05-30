523d9ec424940f1029e3d707451c8c1bd3cb24b7
wasifjhojha-boop <wasifjhojha@gmail.com>
2026-05-30 15:16:07 +0530

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,7 +3,14 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src')
     },
-  },
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        '@': path.resolve(__dirname, 'src')
+      };
+      return webpackConfig;
+    }
+  }
 };
